aa389a584e8bc79c869bf9ce1720a55c6fc420bb
Luis Jesus <luisjesusavilesmorales7@gmail.com>
2024-07-22 12:59:41 -0700

    fix referencia a archivos js en blade

--- src/resources/views/api-documentation.blade.php
+++ src/resources/views/api-documentation.blade.php
@@ -208,8 +208,8 @@ POST http://127.0.0.1:8000/{{$response['originalRequest']['url']['path'][0] ?? n
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
       <script src="https://malihu.github.io/custom-scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
-      <script src="{{ asset('assets/js/bootstrap-treeview.min.js') }} "></script>
-      <script src="{{ asset('assets/js/main.min.js') }} "></script>
+      <script src="{{ asset('postman-collection-viewer-assets/js/bootstrap-treeview.min.js') }} "></script>
+      <script src="{{ asset('postman-collection-viewer-assets/js/main.min.js') }} "></script>
       <script type="application/javascript">
          var data = @json($sidebarElements);
          $('#tree').treeview({
